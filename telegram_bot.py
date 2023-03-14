f19559d2c1db9a509fb5dc1c41bddd38746c734e
free-flow-code <wildmuskaria8@gmail.com>
2023-03-14 13:41:47 +0400

    Publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,7 +6,11 @@ import telegram
 def hello_bot():
     tg_token = os.environ['TG_TOKEN']
     bot = telegram.Bot(token=tg_token)
-    bot.send_message(text ='Hello world!', chat_id=-1001813053173)
+    #bot.send_message(text ='Hello world!', chat_id=-1001813053173)
+    #bot.send_chat_action(chat_id=-1001813053173, action=message.UPLOAD_PHOTO)
+    bot.send_photo(-1001813053173,
+                   'https://github.com/free-flow-code/SpaceInTelegram/blob/main/image/AndromedaGalex_900.jpg?raw=true'
+                   )
 
 
 if __name__ == '__main__':
