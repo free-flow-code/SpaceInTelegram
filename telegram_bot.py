c1f61986fafd5e4f48f1038b5972d35eb23142a3
free-flow-code <wildmuskaria8@gmail.com>
2023-03-14 14:02:53 +0400

    Telegram bot post images from local directory.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,28 @@
 import os
 from dotenv import load_dotenv
 import telegram
+import argparse
 
 
-def hello_bot():
+def create_arguments_parser():
+    """Parse command-line arguments and return user-entered launch ID."""
+    parser = argparse.ArgumentParser(description='Publishes images to telegram channel')
+    parser.add_argument('launch_id', help='Enter file name', nargs='?', default=[''])
+    return parser
+
+
+def post_image():
     tg_token = os.environ['TG_TOKEN']
     bot = telegram.Bot(token=tg_token)
-    #bot.send_message(text ='Hello world!', chat_id=-1001813053173)
-    #bot.send_chat_action(chat_id=-1001813053173, action=message.UPLOAD_PHOTO)
-    bot.send_photo(-1001813053173,
-                   'https://github.com/free-flow-code/SpaceInTelegram/blob/main/image/AndromedaGalex_900.jpg?raw=true'
-                   )
+    parser = create_arguments_parser()
+    arguments = parser.parse_args()
+    file_name = arguments.launch_id[0:]
+    if file_name:
+        bot.send_photo(-1001813053173, photo=open('image/{}'.format(file_name), 'rb'))
+    else:
+        bot.send_photo(-1001813053173, photo=open('image/{}'.format(get_random_image()), 'rb'))
 
 
 if __name__ == '__main__':
     load_dotenv()
-    hello_bot()
+    post_image()
