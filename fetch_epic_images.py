9fd6a4212acc827130b7d8edb8bf67cace627325
free-flow-code <wildmuskaria8@gmail.com>
2023-03-15 17:20:08 +0400

    Removed string addition.

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -6,15 +6,14 @@ from file_processing import *
 def get_epic_images(nasa_api_key):
     """Downloads  Earth Polychromatic Imaging Camera (EPIC) photos"""
     all_images_link = 'https://api.nasa.gov/EPIC/api/natural/images'
-    one_image_link = 'https://api.nasa.gov/EPIC/archive/natural/{}/png/{}'
+    one_image_link = 'https://api.nasa.gov/EPIC/archive/natural/{}/png/{}{}'
     params = {'api_key': nasa_api_key}
     all_images_response = requests.get(all_images_link, params=params)
     all_images_response.raise_for_status()
     for image in all_images_response.json():
         date = datetime.datetime.strptime(image['date'][:10], '%Y-%m-%d').date()
         path = str(date).replace('-', '/')
-        filename = image['image']+'.png'
-        image_link = one_image_link.format(path, filename)
+        image_link = one_image_link.format(path, image['image'], '.png')
         download_image(image_link, params=params)
 
 
