8579f1b56863418549d6701d8d76db6c8391ac59
free-flow-code <wildmuskaria8@gmail.com>
2023-03-15 17:33:24 +0400

    Added default value for command line arguments.
    That allowed to remove an extra function.

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -14,20 +14,13 @@ from file_processing import *
 def create_arguments_parser():
     """Parse command-line arguments and return user-entered launch ID."""
     parser = argparse.ArgumentParser(description='Download images of SpaceX launches')
-    parser.add_argument('launch_id', help='Enter launch ID', nargs='?', default='')
+    parser.add_argument('launch_id', help='Enter launch ID', nargs='?', default='latest')
     return parser
 
 
-def get_id_images_links(launch_id):
-    site = 'https://api.spacexdata.com/v5/launches/{}'
-    id_response = requests.get(site.format(launch_id))
-    id_response.raise_for_status()
-    return id_response.json()['links']['flickr']['original']
-
-
-def get_ll_images_links():
-    latest_launch = 'https://api.spacexdata.com/v5/launches/latest'
-    ll_response = requests.get(latest_launch)
+def get_spacex_images_links(launch_id):
+    site = 'https://api.spacexdata.com/v5/launches/{}'.format(launch_id)
+    ll_response = requests.get(site)
     ll_response.raise_for_status()
     return ll_response.json()['links']['flickr']['original']
 
@@ -36,13 +29,8 @@ def get_spacex_images():
     parser = create_arguments_parser()
     arguments = parser.parse_args()
     launch_id = arguments.launch_id
-    if launch_id:
-        for image_link in get_id_images_links(launch_id):
-            download_image(image_link)
-    else:
-        # ll - latest launch
-        for image_link in get_ll_images_links():
-            download_image(image_link)
+    for image_link in get_spacex_images_links(launch_id):
+        download_image(image_link)
 
 
 if __name__ == '__main__':
