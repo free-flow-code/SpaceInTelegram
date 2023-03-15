ccc19dc7f5e1d4905d283b61b6a373159996dea9
free-flow-code <wildmuskaria8@gmail.com>
2023-03-15 15:59:35 +0400

    Changed accepted data type for command line arguments.

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,7 +10,7 @@ from file_processing import get_random_image
 def create_arguments_parser():
     """Parse command-line arguments and return user-entered image file name."""
     parser = argparse.ArgumentParser(description='Publishes images to telegram channel')
-    parser.add_argument('file_name', help='Enter file name', nargs='?', default=[''])
+    parser.add_argument('file_name', help='Enter file name', nargs='?', default='')
     return parser
 
 
@@ -18,8 +18,8 @@ def post_image(tg_token, chat_id, file_name=None):
     bot = telegram.Bot(token=tg_token)
     parser = create_arguments_parser()
     arguments = parser.parse_args()
-    if arguments.file_name[0:0]:
-        file_name = arguments.file_name[0:0]
+    if arguments.file_name:
+        file_name = arguments.file_name
         with open('image/{}'.format(file_name), 'rb') as file:
             bot.send_photo(chat_id, photo=file)
     else:
