1314786777084fbb0edddf61a985488deb56f4ad
free-flow-code <wildmuskaria8@gmail.com>
2023-03-15 23:51:01 +0400

    The name of the variable for the get request has been changed.

--- fetch_apod_images.py
+++ fetch_apod_images.py
@@ -5,9 +5,9 @@ from file_processing import *
 def get_apod_images(nasa_api_key):
     """Downloads Astronomy Picture of the Day (APOD)"""
     apod_link = 'https://api.nasa.gov/planetary/apod'
-    number_download_images = 30
+    number_images_download = 30
     params = {'api_key': nasa_api_key,
-              'count': number_download_images}
+              'count': number_images_download}
     apod_response = requests.get(apod_link, params=params)
     apod_response.raise_for_status()
     images = apod_response.json()
